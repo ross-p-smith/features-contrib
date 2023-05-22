08e40aeb280960838f5f91ada3ee8ce6d5b97d17
ross-p-smith <rosmith@microsoft.com>
2023-05-22 21:32:06 +0100

    UID 1000

--- test/porter/install_mixins.sh
+++ test/porter/install_mixins.sh
@@ -4,18 +4,15 @@ set -e
 
 source dev-container-features-test-lib
 
-check "porter --version" porter --version
-
 echo "PORTER_HOME: $PORTER_HOME"
 echo "User: $(whoami)"
 echo "UserId: $(id -u)"
 echo "GroupId: $(id -g)"
 echo "Remote User: $_REMOTE_USER"
-
-# We should not have to do this!!
-chown -R vscode "$PORTER_HOME"
-
+echo "Remote User: $REMOTE_USER"
 ls -al "$PORTER_HOME"
+
+check "porter --version" porter --version
 
 check "exec mixin installed" "$PORTER_HOME"/mixins/exec/exec version
 check "terraform mixin installed" "$PORTER_HOME"/mixins/terraform/terraform version
