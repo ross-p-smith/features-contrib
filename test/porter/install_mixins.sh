1ef6f9392bd7da3f6483a417918ad47a6c23efd1
ross-p-smith <rosmith@microsoft.com>
2023-05-22 16:57:56 +0100

    Debug why mixin check doesn't have permission

--- test/porter/install_mixins.sh
+++ test/porter/install_mixins.sh
@@ -6,6 +6,30 @@ source dev-container-features-test-lib
 
 check "porter --version" porter --version
 
+# Old check with grep
+check "porter mixin list | grep terraform" porter mixin list | grep terraform
+
+check "porter mixin list | grep az" porter mixin list | grep az
+
+check "porter mixin list | grep aws" porter mixin list | grep aws
+
+check "porter mixin list | grep docker" porter mixin list | grep docker
+
+check "porter mixin list | grep docker-compose" porter mixin list | grep docker-compose
+
+check "porter mixin list | grep gcloud" porter mixin list | grep gcloud
+
+check "porter mixin list | grep helm" porter mixin list | grep helm
+
+check "porter mixin list | grep arm" porter mixin list | grep arm
+
+check "porter plugin list | grep azure" porter plugin list | grep azure
+
+check "porter plugin list | grep kubernetes" porter plugin list | grep kubernetes
+
+# Check File permissions in GH Actions. (Works locally, but not in GH Actions)
+ls -al "$PORTER_HOME/mixins/exec"
+
 check "exec mixin installed" "$PORTER_HOME"/mixins/exec/exec version
 check "terraform mixin installed" "$PORTER_HOME"/mixins/terraform/terraform version
 check "az mixin installed" "$PORTER_HOME"/mixins/az/az version
